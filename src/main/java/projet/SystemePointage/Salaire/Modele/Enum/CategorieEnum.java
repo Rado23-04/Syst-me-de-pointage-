50391a4b2b831b3c432d90c10ab5b9d835228377
rado23-04 <hei.rado.2@gmail.com>
2024-06-29 13:50:12 +0300

    fix: method guardian is works night

--- src/main/java/projet/SystemePointage/Salaire/Modele/Enum/CategorieEnum.java
+++ src/main/java/projet/SystemePointage/Salaire/Modele/Enum/CategorieEnum.java
@@ -3,6 +3,17 @@ package projet.SystemePointage.Salaire.Modele.Enum;
 public enum CategorieEnum {
     SUPERIOR_FRAME,
     NORMAL,
-    GUARDIAN,
+    GUARDIAN(true),
     DRIVER;
+    private boolean worksAtNight;
+    CategorieEnum() {
+        this.worksAtNight = false;
+    }
+    CategorieEnum(boolean worksAtNight) {
+        this.worksAtNight = worksAtNight;
+    }
+    public boolean isWorksAtNight() {
+        return worksAtNight;
+    }
 }
+//CategorieEnum DAY_GUARDIAN = new CategorieEnum(false);
